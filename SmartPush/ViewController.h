9089e7c1739f9eaac02d16173e74f102a72eecd5
Amit Saurav <amitsaurav@gmail.com>
2015-07-01 12:42:07 -0700

    Initial Commit

---
+++ SmartPush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartPush
+//
+//  Created by Saurav, Amit on 7/1/15.
+//  Copyright (c) 2015 Saurav, Amit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmartPush/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartPush
+//
+//  Created by Saurav, Amit on 7/1/15.
+//  Copyright (c) 2015 Saurav, Amit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
